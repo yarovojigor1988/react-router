e0c77bcc17dd90ab54da8a6309b308296f11bced
yarovojigor1988 <yarovojigo1988@gmail.com>
2021-11-25 11:07:52 +0200

    React router

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, {Component} from 'react'
+import './App.scss'
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
+import About from './About/About'
+import Cars from './Cars/Cars'
+import CarDetail from './CarDetail/CarDetail'
+
+class App extends Component {
+
+  state = {
+    isLoggedIn: false,
+  }
+  render() {
+
+    return (
+      <div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <NavLink 
+                to="/" 
+                exact 
+                activeClassName={'active'}
+              >Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" activeStyle={{color:'green'}}>About</NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to={{
+                  pathname: '/cars',
+                }} 
+              >Cars</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <hr />
+        <div style={{textAlign:'center'}}>
+          <h3>Is logged {this.state.isLoggedIn ? 'TRUE' : 'FALSE'}</h3>
+          <button onClick={()=>this.setState({isLoggedIn:true})}>Login</button>
+        </div>
+
+
+        <hr/>
+        <Switch>
+          <Route 
+            path='/'
+            exact
+            render={()=><h1>Home Page</h1>}
+          />
+
+
+
+          { this.state.isLoggedIn 
+          ? <Route 
+            path='/about'
+            exact
+            component={About}
+          /> 
+          : null}
+          <Route 
+            path='/cars/:name'          
+            component={CarDetail}
+          />
+
+          <Route 
+            path='/cars'
+            component={Cars}
+          />  
+          <Redirect to={'/'}/>
+          {/* <Route render={()=><h1 style={{color:'red',textAlign:'center'}}>404 not found</h1>} /> */}
+        </Switch>
+
+
+      </div>
+    );
+  }
+}
+
+export default App
